701455b0d9e09abf25fc67fc1cd13b0020031045
Gaurav Parwani <gparwani15@gmail.com>
2024-06-17 19:20:11 +0530

    commit 2 testing of server from 360 laptop

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express();
 
 const port = process.env.port || 5000;
 
+app.get('/', (req, res)=>{
+    res.status(200);
+    res.send("Welcome to root URL of Server");
+});
+
 app.listen(port, () => {
     console.log(`Listening on ${port}`);
 });
